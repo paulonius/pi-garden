f1d5dadb223d3c5d0421118759aa77eec538c366
Pablo Hernandez-Ferreiros <pablohf@programgeo.net>
2019-07-11 18:05:34 +0200

    Added delayer script for sunwait and deleted climate

--- batch/climate.py
+++
@@ -1,6 +0,0 @@
-from api.persistence import connect, config
-from api.model import Climate
-from api.sensors import getDHTdata
-from api.external import getLocalClimate
-
-
---
+++ batch/delayer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+usage() 
+{
+    echo "usage: delayer.sh <sunrise|sundown> <script>"
+}
+
+if [ $# -lt 2 ]; then
+    usage
+    exit 1
+else 
+    case $1 in
+        sunrise) 
+            period=rise
+            ;;
+        sunset)
+            period=set
+            ;;
+        * )                 
+            usage
+            exit 1
+    esac
+fi
+
+echo $PERIOD
+sunwait wait civ $period 42.342742N 7.858719W
+if [ $? = 1 ]; then
+    shift
+    $@
+else
+    echo "too late to execute for sun$period"
+    exit 0
+fi
