d255f985dc740661684f402e8abd0237baba79cd
Pablo Hernandez-Ferreiros <pablohf@programgeo.net>
2019-07-13 23:13:17 +0200

    Made batch scripts not depend on pipenv
    Pipenv is much too slow for normal batch scripts. I use this method to
    prevent depending on them

--- batch/pump.py
+++ batch/pump.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 import pigpio
-from api.persistence import config
+import configparser
+import os
+
+dirname = os.path.dirname(__file__)
+if "pi-garden" in dirname:
+    config_file = os.path.join(dirname, '../config.ini')
+else:
+    config_file = os.path.join(os.environ['HOME'], 'pi-garden/config.ini')
+
+config = configparser.ConfigParser()
+config.read(config_file)
 
 SIG = int(config['SENSORS']['Pump'])
 
--- batch/sonar.py
+++ batch/sonar.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import time
 import pigpio
+import configparser
+import os
 
-from api.persistence import config
+dirname = os.path.dirname(__file__)
+if "pi-garden" in dirname:
+    config_file = os.path.join(dirname, '../config.ini')
+else:
+    config_file = os.path.join(os.environ['HOME'], 'pi-garden/config.ini')
+
+config = configparser.ConfigParser()
+config.read(config_file)
 
 TRIGGER = int(config['SENSORS']['SonarTrigger'])
 ECHO = int(config['SENSORS']['SonarEcho'])
